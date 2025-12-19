ebafce9a6ef434ca3b29a0fbe81ffb3545a60ddb
Abdulakhad Khadjimetov <abdulaxad575@gmail.com>
2025-12-20 00:26:07 +0500

    feat: remove unused path and URL imports from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    // Указывает Next.js рассматривать родительский каталог как корень трассировки
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
-  // ... другие ваши настройки Next.js
+  /* config options here */
 };
 
 export default nextConfig;
