a744a5a47a423859e6602489b7ac59212846ce5d
Abdulakhad Khadjimetov <abdulaxad575@gmail.com>
2025-12-20 00:12:49 +0500

    feat: add experimental output file tracing configuration in Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,13 @@ const nextConfig = {
   /* config options here */
 };
 
+const path = require('path');
+
+module.exports = {
+  experimental: {
+    // Указывает Next.js рассматривать родительский каталог как корень трассировки
+    outputFileTracingRoot: path.join(__dirname, '../'),
+  },
+  // ... другие ваши настройки Next.js
+};
 export default nextConfig;
