a3f49c8dc28e3ef8d19c9ce5dd1223718eb082d9
Abdulakhad Khadjimetov <abdulaxad575@gmail.com>
2025-12-20 00:21:49 +0500

    feat: refactor next.config.mjs to use ES module syntax and improve path handling

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
-
-const path = require('path');
-
-module.exports = {
   experimental: {
     // Указывает Next.js рассматривать родительский каталог как корень трассировки
     outputFileTracingRoot: path.join(__dirname, '../'),
   },
   // ... другие ваши настройки Next.js
 };
+
 export default nextConfig;
