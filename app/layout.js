86d96f9e10035f21b49ab598ac513947b62d0b92
Abdulakhad Khadjimetov <abdulaxad575@gmail.com>
2025-12-21 00:38:07 +0500

    fix: correct application name in metadata description

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 
 export const metadata = {
   title: "Geo-Note",
-  description: "GeoNote is a modern, full-stack web application designed for efficient team collaboration, meeting management, and personal note-taking",
+  description: "Geo-Note is a modern, full-stack web application designed for efficient team collaboration, meeting management, and personal note-taking",
 };
 
 export default function RootLayout({ children }) {
