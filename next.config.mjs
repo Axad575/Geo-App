74099d4beb2fe51d97417eb575ce19653357e249
Abdulakhad Khadjimetov <abdulaxad575@gmail.com>
2025-12-21 00:39:15 +0500

    refactor: remove deprecated server setup and clean up configuration files

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Укажите правильный корневой каталог
-  turbopack: {
-    root: '. /',
-  },
-  // Отключите минификацию при сборке для экономии памяти
-  swcMinify: false,
-  // Уменьшите использование памяти
-  experimental: {
-    workerThreads: false,
-    cpus: 1
-  }
 }
 
 export default nextConfig
