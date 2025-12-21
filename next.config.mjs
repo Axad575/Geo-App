3dcc03ffe45536e5162a1252fae122b5a9f9db7b
Abdulakhad Khadjimetov <abdulaxad575@gmail.com>
2025-12-21 22:56:06 +0500

    refactor: remove experimental turbo configuration from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,9 @@
-import { join } from 'path';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compress: true,
   poweredByHeader: false,
   // Для production
   reactStrictMode: true,
-
-  experimental:{
-    turbo:{
-      root: join(__dirname, '..')
-    }
-  }
 }
 
 export default nextConfig
