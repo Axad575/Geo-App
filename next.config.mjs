44316c28df1b738b7665da0cd8cc79e071d37b20
Abdulakhad Khadjimetov <abdulaxad575@gmail.com>
2025-12-21 22:52:31 +0500

    feat: enhance next.config.mjs with experimental turbo configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
+import { join } from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compress: true,
   poweredByHeader: false,
   // Для production
   reactStrictMode: true,
+
+  experimental:{
+    turbo:{
+      root: join(__dirname, '..')
+    }
+  }
 }
 
 export default nextConfig
